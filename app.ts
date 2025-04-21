c0583fe1da199b3fb79c1158dbf503a2696b57be
Andrei Timotin <timotin.andrei@justconsult.md>
2025-04-21 13:48:59 +0300

    Типы в функциях

--- app.ts
+++ app.ts
@@ -3,3 +3,11 @@ let bonus: number = 500;
 
 let res: number = revenue + bonus;
 console.log(res)
+
+function getFullName(firstName: string, lastName: string): string {
+    return `${firstName} ${lastName}`;
+}
+
+const getFullNameArrow = (firstName: string, lastName: string): string => {
+    return `${firstName} ${lastName}`;
+}
